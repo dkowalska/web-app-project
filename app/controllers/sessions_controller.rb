2d90ba94b81e5abc57ef911897332f4119d72258
dkowalska <dariaa.kowalska@gmail.com>
2016-02-06 15:10:53 +0000

    Add remember digest migration

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,7 @@ class SessionsController < ApplicationController
       log_in user
       redirect_to user
     else
-      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
-      # Create an error message.
+      flash.now[:danger] = 'Invalid email/password combination' 
       render 'new'
     end
   end
