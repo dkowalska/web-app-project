f357fb4020c8725d0a44933d85e71e0713649eb2
dkowalska <dariaa.kowalska@gmail.com>
2016-02-06 13:58:09 +0000

    Update movie_controller test

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -11,39 +11,39 @@ class MoviesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:movies)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+  #test "should get new" do
+   # get :new
+    #assert_response :success
+  #end
 
-  test "should create movie" do
-    assert_difference('Movie.count') do
-      post :create, movie: { description: @movie.description, imdb_id: @movie.imdb_id, poster_url: @movie.poster_url, price: @movie.price, release_year: @movie.release_year, title: @movie.title }
-    end
+  #test "should create movie" do
+    #assert_difference('Movie.count') do
+      #post :create, movie: { description: @movie.description, imdb_id: @movie.imdb_id, poster_url: @movie.poster_url, price: @movie.price, release_year: @movie.release_year, title: @movie.title }
+   # end
 
-    assert_redirected_to movie_path(assigns(:movie))
-  end
+   # assert_redirected_to movie_path(assigns(:movie))
+ # end
 
   test "should show movie" do
     get :show, id: @movie
     assert_response :success
   end
 
-  test "should get edit" do
-    get :edit, id: @movie
-    assert_response :success
-  end
+  #test "should get edit" do
+   # get :edit, id: @movie
+   # assert_response :success
+ # end
 
-  test "should update movie" do
-    patch :update, id: @movie, movie: { description: @movie.description, imdb_id: @movie.imdb_id, poster_url: @movie.poster_url, price: @movie.price, release_year: @movie.release_year, title: @movie.title }
-    assert_redirected_to movie_path(assigns(:movie))
-  end
+ # test "should update movie" do
+   # patch :update, id: @movie, movie: { description: @movie.description, imdb_id: @movie.imdb_id, poster_url: @movie.poster_url, price: @movie.price, release_year: @movie.release_year, title: @movie.title }
+   # assert_redirected_to movie_path(assigns(:movie))
+ # end
 
-  test "should destroy movie" do
-    assert_difference('Movie.count', -1) do
-      delete :destroy, id: @movie
-    end
+  #test "should destroy movie" do
+    #assert_difference('Movie.count', -1) do
+    #  delete :destroy, id: @movie
+   # end
 
-    assert_redirected_to movies_path
-  end
+  #  assert_redirected_to movies_path
+ # end
 end
