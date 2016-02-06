51e4dd32666c54cd0bd4b0108b2720df0dc98b77
dkowalska <dariaa.kowalska@gmail.com>
2016-02-06 21:18:10 +0000

    Add update user, admin, delete user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,20 @@
 require 'csv'
 CSV.foreach(Rails.root.join("db/seeds_data/movies.csv"), headers: true) do |row|
-  Movie.find_or_create_by(title: row[0], release_year: row[1], price: row[2], description: row[3], imdb_id: row[4], poster_url: row[5])
+  Movie.find_or_create_by(title: row[0], release_year: row[1], price: row[2], description: row[3], imdb_id: row[4], poster_url: row[5], runtime: row[6], director: row[7], genre: row[8], country: row[9])
+end
+
+User.create!(name:  "admin",
+             email: "admin@example.com",
+             password:              "administrator",
+             password_confirmation: "administrator",
+             admin: true)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
 end
